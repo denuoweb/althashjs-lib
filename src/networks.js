0801cee2ab2bf6b6c8118ec1b4e5a6750a7ddd8d
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-05-11 19:31:36 -0700

    update keys

--- src/networks.js
+++ src/networks.js
@@ -6,19 +6,8 @@ module.exports = {
             public: 0x0488b21e,
             private: 0x0488ade4
         },
-        pubKeyHash: 0x3a,
-        scriptHash: 0x32,
+        pubKeyHash: 0x29,
+        scriptHash: 0x64,
         wif: 0x80
-    },
-    htmlcoin_testnet: {
-        messagePrefix: '\x15Htmlcoin Signed Message:\n',
-        bech32: 'tb',
-        bip32: {
-            public: 0x043587cf,
-            private: 0x04358394
-        },
-        pubKeyHash: 0x78,
-        scriptHash: 0x6e,
-        wif: 0xef
     }
 }
