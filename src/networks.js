44b256500d8842d16f829c02fa28b17d942a2fa7
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-06-13 00:18:42 -0700

    update testnet

--- src/networks.js
+++ src/networks.js
@@ -9,5 +9,16 @@ module.exports = {
         pubKeyHash: 0x29,
         scriptHash: 0x64,
         wif: 0x80
+    },
+    htmlcoin_testnet: {
+        messagePrefix: '\x19Htmlcoin Signed Message:\n',
+        bech32: 'tb',
+        bip32: {
+            public: 0x043587cf,
+            private: 0x04358394
+        },
+        pubKeyHash: 0x100,
+        scriptHash: 0x6e,
+        wif: 0xef
     }
 }
